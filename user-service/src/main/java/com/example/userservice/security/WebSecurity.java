49fee0d715d4e814699b3682c04de64b0744736f
Youhoseong <h0song@naver.com>
2022-01-23 15:25:53 +0900

    [Feat] User Spring Security 적용

---
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -0,0 +1,24 @@
+package com.example.userservice.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // configure은 인증과 권한에 대한 것이 있다. => 이것은 인증은 됐다고 가정하고 권한에 대한 정의임.
+
+        http.csrf().disable();
+        http.authorizeHttpRequests().antMatchers("/user/**").permitAll();
+
+
+        http.headers().frameOptions().disable();
+
+    }
+}
